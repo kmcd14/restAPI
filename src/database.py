10877521d6ab3f4a824dbc8bb3f404bd8ffd5d05
Katie Mc Donald <katieisanimdom@gmail.com>
2022-11-15 22:37:59 +0000

    fixing database email variable error

--- src/database.py
+++ src/database.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os 
 
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
 db_path = os.path.join(os.path.dirname(__file__), 'bookmarks.db')
 db_uri = 'sqlite:///{}'.format(db_path)
@@ -11,14 +11,13 @@ db_uri = 'sqlite:///{}'.format(db_path)
 
   # Config database
 #app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 # Creating database tables
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.Text(), nullable=False)
     created = db.Column(db.DateTime, default=datetime.now())
     updated  = db.Column(db.DateTime, onupdate=datetime.now())
