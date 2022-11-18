a399022de92d476929eb91d80548f3c89a51956b
Katie Mc Donald <katieisanimdom@gmail.com>
2022-11-18 18:57:42 +0000

    creating route to update and delete bookmarks

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -13,7 +13,7 @@ bookmarks = Blueprint("bookmarks", __name__, url_prefix="/api/bookmarks")
 @bookmarks.route("/", methods=['POST', 'GET'])
 
 def post_bookmark():
-    
+   
 
     if request.method == 'POST':
 
@@ -36,7 +36,7 @@ def post_bookmark():
 
     else:
 
-        
+       
 
         data = []
         for bookmark in bookmarks:
@@ -51,7 +51,7 @@ def post_bookmark():
 @bookmarks.get("/<int:id>")
 
 def get_one(id):
-    
+
 
     bookmark = Bookmark.query.filter_by( id=id).first()
 
@@ -63,6 +63,47 @@ def get_one(id):
 
 
 
+# Update bookmark
+@bookmarks.put('/<int:id>')
 
 
+def update_bookmark(id):
 
+
+    bookmark = Bookmark.query.filter_by(id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'bookmark not found'}), HTTP_404_NOT_FOUND
+
+    description = request.get_json().get('description', '')
+    url = request.get_json().get('url', '')
+
+    if not validators.url(url):
+        return jsonify({'error': 'invalid url'}), HTTP_400_BAD_REQUEST
+
+    bookmark.url = url
+    bookmark.description = description
+
+    db.session.commit() 
+
+
+    return jsonify({'id': bookmark.id,'url': bookmark.url,'visit': bookmark.visited,'description': bookmark.description,
+            'created': bookmark.created,'updated': bookmark.updated,}), HTTP_200_OK
+
+
+# Delete bookmark
+@bookmarks.delete('/<int:id>')
+
+def delete_bookmark(id):
+
+
+    bookmark = Bookmark.query.filter_by(id=id).first()
+
+    if not bookmark:
+        return jsonify({'message': 'bookmark not found'}), HTTP_404_NOT_FOUND
+
+
+    db.session.delete(bookmark)
+    db.session.commit()
+
+    return jsonify({"message": "bookmark deleted"})
