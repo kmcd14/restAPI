dcb78ad5d38f974497050f638fcf52bf2d8b398b
Katie Mc Donald <katieisanimdom@gmail.com>
2022-12-08 21:28:24 +0000

    testing app routes are working

--- application.py
+++ application.py
@@ -2,5 +2,32 @@ from flask import Flask, jsonify, abort
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
+
+bookmarks = [
+    {'name':'www.twitter.com'},
+    {'name':'www.google.com'},
+    {'name':'www.github.com'},
+    ]
+
+
+# Homepage
+@app.route('/')
+def homepage():
+    return jsonify({'message': 'home'})
+
+
+# Get bookmarks
+@app.route("/bookmarks", methods=['GET'])
+def getAllBookmarks():
+        return jsonify(bookmarks)
+
+
+# Delete 
+@app.route('/vote/all', methods=['DELETE'])
+def deleteBookmark():
+    return jsonify({'done':True})
+
+
+
 if __name__ == "__main__":
   app.run(debug=True)
